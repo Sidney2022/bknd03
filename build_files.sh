22f8ea5575a88050946d66b5bb17577753b7f4af
Sidney2022 <uwayasidney@gmail.com>
2024-07-08 00:06:52 +0100

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,7 @@
 # build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+# pip install -r requirements.txt
+# python3.9 manage.py collectstatic --noinput
+
+
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput
