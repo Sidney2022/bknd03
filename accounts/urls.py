53627bd75f7caf35b067a53c7e12790eca6c9a63
Sidney2022 <uwayasidney@gmail.com>
2024-07-08 11:45:26 +0100

    /api/

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('/register', views.RegisterView.as_view(), name='register'),
-    # path('auth/login', views.LoginView.as_view(), name='login'),
+    path('auth/register', views.RegisterView.as_view(), ),
+    path('auth/login', views.LoginView.as_view(), ),
     path('users/<str:id>/', views.UserDetailView.as_view(), name='user-detail'),
     path('organisations', views.OrganisationListView.as_view(), name='organisation-list'),
     path('organisations/<str:org_id>', views.OrganisationDetailView.as_view(), name='organisation-detail'),
